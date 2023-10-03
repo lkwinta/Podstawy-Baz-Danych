f4134c276fad0cbfdd12af12de12c7f2cc43243d
kwinta <kwinta@d17-428-pc-12.ipa.ii.agh.edu.pl>
2023-10-03 16:29:28 +0200

    lab 1 queries

--- lab1/lab1.sql
+++ lab1/lab1.sql
@@ -1,4 +1,13 @@
-SELECT *
-FROM Customers
-WHERE Customers.City = 'London'
-ORDER BY ContactName;
+-- Test query
+SELECT * FROM Products
+
+-- Print out version of the server
+SELECT @@VERSION
+
+SELECT * FROM Customers
+SELECT * FROM Suppliers
+SELECT * FROM Categories
+
+-- Use "", [] to select from table named with space
+SELECT * FROM [Order Details] WHERE OrderID = 10250
+SELECT * FROM "Order Details" WHERE OrderID = 10250
